c9a1f7c2f69e2adc6653d31f56a1f29a0ea660cd
PiotrBerezowski <piotr.berezowski@fdmgroup.com>
2015-12-03 17:13:49 +0000

    Added Viewmodels and cleaned up extra files

---
+++ Rebellion/RebelInterface/AddRebelViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RebelGooey
+{
+    public class AddRebelViewModel : INotifyPropertyChanged
+    {
+        private int _rebelId;
+        public int rebelId
+        {
+            get { return _rebelId; }
+            set { 
+                _rebelId = value;
+                onPropertyChanged("rebelId");
+            }
+        }
+
+        private string _name;
+        public string name
+        {
+            get { return _name; }
+            set { _name = value;
+              On
+            }
+        }
+        
+        private int _territoryId;
+        public int territoryId
+        {
+            get { return _territoryId; }
+            set { _territoryId = value; onPropertyChanged("territoryId"); }
+        }
+        
+
+        
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void onPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
