60a3fe3df9d94ae49020ebefd12e31e2c973460d
hmohamed7 <hannah.mohamed@fdmgroup.com>
2015-12-03 17:18:40 +0000

    Delete and Select functionality added

---
+++ Rebellion/RebellionDataBaseLibrary/Select/SelectGroupByID.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RebellionDataBaseLibrary.Select
+{
+    class SelectGroupByID
+    {
+        public static string FindGroup(int id)
+        {
+            try
+            {
+                int ID = id;
+                string groupName = String.Empty;
+                using (var context = new RebellionDataEntities())
+                {
+                    groupName = context.Groups.Find(ID).group_name;
+                }
+                return groupName;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
