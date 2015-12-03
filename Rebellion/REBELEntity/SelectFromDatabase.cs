88b96f9d76b852645bd747c6bbc9b0ab0f32fb72
hmohamed7 <hannah.mohamed@fdmgroup.com>
2015-12-03 16:01:57 +0000

    Slight alterations to the database

--- Rebellion/REBELEntity/SelectFromDatabase.cs
+++ Rebellion/REBELEntity/SelectFromDatabase.cs
@@ -3,16 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RebellionDataBaseModel;
 
 namespace REBELEntity
 {
     class SelectFromDatabase
     {
+
         public SelectFromDatabase()
         {
             //Each Select statement requires
-            //SELECT WHAT
-            //FROM WHERE
+            //SELECT WHAT eg first_name, last_name
+            //FROM WHERE eg table - We do not allow the user to select which table
+            //therefore it must be set in the class
+            //But there MUST be a table and at least ONE thing to select
+
+            //using (var context = new PokeBaseEntities())
+            //{
+            //    var queryResult = (from p in context.Pokemons
+            //                       where p.type == "Fire"
+            //                       select p);
+
+            //    foreach (var pokemon in queryResult)
+            //    {
+            //        Console.WriteLine(pokemon.name);
+            //    }
+            //}
+
+
+
         }
+
+   
+        
+
+        
     }
 }
--- Rebellion/RebellionDataBaseLibrary/RebellionDataModel.Designer.cs
+++ Rebellion/RebellionDataBaseLibrary/RebellionDataModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\hannah.mohamed\Desktop\Rebel\Rebellion\Rebellion\Model.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\hannah.mohamed\Desktop\Rebel\Rebellion\RebellionDataBaseLibrary\RebellionDataModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
