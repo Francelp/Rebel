21d0b46a59f4620086899a7ef11c4992aa250766
Francelp <francel_piedra@hotmail.com>
2015-12-04 10:55:36 +0000

    Sync

--- Rebellion/RebelInterface/TempViewModel.cs
+++ Rebellion/RebelInterface/TempViewModel.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
-namespace RebelGooey
+namespace RebelInterface
 {
     public class TempViewModel : INotifyPropertyChanged
     {
